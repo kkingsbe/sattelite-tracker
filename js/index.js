2d04d56ea93d4c6f9f121d2c9990f67cfffbac26
Kyle <kkingsbe@gmail.com>
2020-05-09 11:50:35 -0400

    First

---
+++ js/index.js
@@ -0,0 +1,84 @@
+var map, markers = []
+
+function createMap() {
+  map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+  })
+  let featureLayer = new ol.layer.Vector({
+    source: new ol.source.Vector()
+  })
+  featureLayer.set("name", "featurelayer")
+  map.addLayer(featureLayer)
+}
+
+//Returns the feature layer
+function getFeatureLayer() {
+  let layers = map.getLayers().array_
+  for(let layer of layers) if(layer.get("name") == "featurelayer") return layer
+}
+
+//Returns true if the given marker exists
+function markerExists(name) {
+  for(let marker of markers) if(marker.values_.name == name) return true
+}
+
+//Returns the given marker
+function getMarker(name) {
+  let featureLayer = getFeatureLayer()
+  for(let marker of featureLayer.getSource().getFeatures()) if(marker.values_.name == name) return marker
+  return false
+}
+
+/* description: First checks to see if the satellite has already been drawn. If it has, then it just updates its position, and if it hasn't, it creates a marker of its position
+ * parameters: name{string}: The name of the satellite that you would like to draw
+ */
+function drawSatellite(name) {
+  if(markerExists(name)) {
+    updateSatellite(name)
+    return
+  }
+
+  //If we have gotten to this point, the satellite has not yet been drawn
+  console.log("Making new marker")
+  let pos = getCurrentLatLong(name)
+
+  let featureLayer = getFeatureLayer()
+
+  console.log(featureLayer)
+  let marker = new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([pos.long, pos.lat])),
+    name: name
+  })
+  markers.push(marker)
+  featureLayer.getSource().addFeature(marker)
+}
+
+/* description: Updates the position of an existing satellite marker
+ * parameters: name{string}: The name of the satellite that you would like to draw
+ */
+function updateSatellite(name) {
+  //If we have gotten to this point, the satellite has not yet been drawn
+  let pos = getCurrentLatLong(name)
+  console.log(pos)
+  let featureLayer = getFeatureLayer()
+  let marker = getMarker(name)
+  featureLayer.getSource().removeFeature(marker)
+  marker.setGeometry(new ol.geom.Point(ol.proj.fromLonLat([pos.long, pos.lat])))
+  featureLayer.getSource().addFeature(marker)
+  console.log(marker)
+}
+
+createMap()
+
+setInterval(() => {
+  drawSatellite("ISS (ZARYA)")
+}, 1000)
---
+++ js/parse.js
@@ -0,0 +1,109 @@
+/* description: Parses a string of TLEs into an array of tle objects, which follow the format of {name, line1, line2}
+ * parameters: tles{string}: A string of TLEs
+ * return: tleArr{array}: An array of TLE objects
+ */
+function parseTles(tles) {
+  let tleArr = []
+  linesArr = tles.split("\n")
+  let tle = "placeholder"
+  linesArr.forEach(line => {
+    if(line.split(" ")[0] === "1") {
+      tle.line1 = line
+    }
+    else if(line.split(" ")[0] === "2") {
+      tle.line2 = line
+    }
+    else {
+      if(tle !== "placeholder") tleArr.push(tle)
+      tle = {}
+      tle.name = line
+    }
+  })
+  return tleArr
+}
+
+/* description: Calculates the lat/long/height for the given TLE name
+ * parameters: name{string}: The name of the TLE
+ * return: latlon{object}: An object containing the sattelites lat/long/height
+ */
+function getCurrentLatLong(name) {
+  let tle = {}
+  for(let item of tleArr) {
+    if(item.name == name) {
+      tle = item
+      break
+    }
+  }
+  try {
+    let satrec = satellite.twoline2satrec(tle.line1, tle.line2)
+
+    //  Or you can use a JavaScript Date
+    var positionAndVelocity = satellite.propagate(satrec, new Date())
+    
+    // The position_velocity result is a key-value pair of ECI coordinates.
+    // These are the base results from which all other coordinates are derived.
+    var positionEci = positionAndVelocity.position,
+        velocityEci = positionAndVelocity.velocity
+    
+    // You will need GMST for some of the coordinate transforms.
+    // http://en.wikipedia.org/wiki/Sidereal_time#Definition
+    var gmst = satellite.gstime(new Date());
+    
+    var positionEcf   = satellite.eciToEcf(positionEci, gmst),
+        positionGd    = satellite.eciToGeodetic(positionEci, gmst)
+    
+    // Geodetic coords are accessed via `longitude`, `latitude`, `height`.
+    var longitude = positionGd.longitude,
+        latitude  = positionGd.latitude,
+        height    = positionGd.height
+    
+    //  Convert the RADIANS to DEGREES for pretty printing (appends "N", "S", "E", "W", etc).
+    var longitudeStr = satellite.degreesLong(longitude),
+        latitudeStr  = satellite.degreesLat(latitude)
+
+    console.log(longitudeStr)
+    
+    return {lat: latitudeStr, long: longitudeStr, height: height}
+  } catch(e) {
+    throw(e)
+  }
+}
+
+/* description: Converts tleArr to be an array of current sattelite positions
+ * return: posArr{arr}: An array of pos objects with the format {name, lat, long, height}
+ */
+function tleArrToPosArr() {
+  let posArr = []
+  tleArr.forEach(tle => {
+    try{
+      let pos = getCurrentLatLong(tle.name)
+      posArr.push({
+        name: tle.name,
+        lat: pos.lat,
+        long: pos.long,
+        height: pos.height
+      })
+    } catch {
+      console.log(`Error parsing ${tle.name}`)
+    }
+  })
+  return posArr
+}
+
+/* description: Gets an object from the poArr with the specified name
+ * parameters: name{string}: The name of the satellite that you would like to retrieve data for
+ * return: pos{object}: An object of the format {name, lat. long, height}
+ */
+function getPos(name) {
+  let pos = {}
+  for(let item of posArr) {
+    if(item.name == name) {
+      pos = item
+      break
+    }
+  }
+  return pos
+}
+
+var tleArr = parseTles(tles)
+//var posArr = tleArrToPosArr()
